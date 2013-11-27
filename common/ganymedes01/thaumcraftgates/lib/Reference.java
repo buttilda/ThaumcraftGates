419b70d19ea248a39c25064f356045b31b34d028
ganymedes01 <foka_12@hotmail.com>
2013-11-27 23:11:49 +0000

    Remove proxy references

--- common/ganymedes01/thaumcraftgates/lib/Reference.java
+++ common/ganymedes01/thaumcraftgates/lib/Reference.java
@@ -14,7 +14,4 @@ public class Reference {
 	public static final String DEPENDENCIES = "required-after:Forge@[9.10.1.953,);required-after:BuildCraft|Core;required-after:Thaumcraft@[4.0.5,)";
 	public static final String CHANNEL_NAME = "ThaumcraftGates";
 	public static final String VERSION_NUMBER = "0.1";
-
-	public static final String SERVER_PROXY_CLASS = "ganymedes01.thaumcraftgates.core.proxy.CommonProxy";
-	public static final String CLIENT_PROXY_CLASS = "ganymedes01.thaumcraftgates.core.proxy.ClientProxy";
 }
