45945b81882b5a1009215f2e8c2bfebaf3bece1e
ganymedes01 <foka_12@hotmail.com>
2014-05-17 23:04:59 -0300

    Update dependencies

--- src/main/java/ganymedes01/thaumcraftgates/lib/Reference.java
+++ src/main/java/ganymedes01/thaumcraftgates/lib/Reference.java
@@ -11,6 +11,6 @@ public class Reference {
 
 	public static final String MOD_ID = "thaumcraftgates";
 	public static final String MOD_NAME = "Thaumcraft Gates";
-	public static final String DEPENDENCIES = "required-after:Forge@[10.12.0.1056,);after:Thaumcraft";
-	public static final String VERSION_NUMBER = "1.2.0";
+	public static final String DEPENDENCIES = "required-after:Forge@[10.12.0.1056,);required-after:Thaumcraft;required-after:BuildCraft|Core";
+	public static final String VERSION_NUMBER = "1.3.0";
 }
